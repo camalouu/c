e850bf72fb471647b35510b29559466725294249
viktor <camalouu@gmail.com>
2024-01-09 15:39:38 +0500

    1.5 done

---
+++ chapter_1/1.5.c
@@ -0,0 +1,13 @@
+/*
+    Modify the temperature conversion program to print the table in
+    reverse order, that is, from 300 degrees to 0.
+*/
+#include <stdio.h>
+int main()
+{
+    printf("Celsius Fahrenheit\n");
+    // int celsius
+    for(float celsius = 300; celsius>=0; celsius = celsius - 20){
+        printf("%3.0f %10.1f\n", celsius, (9.0 * celsius + 160.0) / 5.0);
+    }
+}
