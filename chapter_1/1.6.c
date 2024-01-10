555d38de25d4f24d45e9af1f6fe780e819789ed0
viktor <camalouu@gmail.com>
2024-01-10 15:02:10 +0500

    1.6 , 1.7 done

---
+++ chapter_1/1.6.c
@@ -0,0 +1,17 @@
+/*
+    Verify that the expression getchar() != EOF is 0 or 1.
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    int c;
+    while (c = getchar() != EOF)
+    {
+        fflush(stdout);
+        usleep(50000);
+        putchar(c);
+    }
+}
