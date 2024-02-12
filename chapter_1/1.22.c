74dcbf921aa98533a231a75df64b592983a21159
viktor <camalouu@gmail.com>
2024-02-12 11:45:15 +0500

    1.22 done

--- chapter_1/1.22.c
+++ chapter_1/1.22.c
@@ -4,52 +4,84 @@
     column of input. Make sure your program does something intelligent with very
     long lines, and if there are no blanks or tabs before the specified column.
 */
-
 #include <stdio.h>
-#define N 10
-#define MAXLINE 1000
 
-int getline_(char line[], int maxlength) {
-  int c, i;
-  for (i = 0; i < maxlength - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
-    line[i] = c;
-  }
+#define MAXCOL 10
+#define TABINC 8
 
-  if (c == '\n')
-    line[i++] = c;
+char line[MAXCOL];
 
-  line[i] = '\0';
+int exptab(int pos);
+int findblnk(int pos);
+int newpos(int pos);
+void printl(int pos);
 
-  return i;
-}
+int main() {
+  char c;
 
-void printFolded(char line[], int len) {
-  int newLineStart = 0;
-  for (int i = 0; i < len; i++) {
-    if (line[i] == ' ') {
-      int j = i;
-      while (line[j++] == ' ') {
-        if (j > newLineStart + N) {
-          putchar('\n');
-          newLineStart = j;
-          break;
-        }
-      }
-      putchar(line[i]);
-    } else {
-      if (i > newLineStart + N) {
-        putchar('\n');
-        newLineStart = i;
-      }
-      putchar(line[i]);
+  int pos = 0;
+
+  while ((c = getchar()) != EOF) {
+    line[pos] = c;
+    if (c == '\t') {
+      pos = exptab(pos);
+    } else if (c == '\n') {
+      printl(pos);
+      pos = 0;
+    } else if (++pos >= MAXCOL) {
+      pos = findblnk(pos);
+      printl(pos);
+      pos = newpos(pos);
     }
   }
 }
 
-int main() {
-  char line[MAXLINE];
-  int len;
-  while ((len = getline_(line, MAXLINE)) > 0) {
-    printFolded(line, len);
+void printl(int pos) {
+  for (int i = 0; i < pos; ++i)
+    putchar(line[i]);
+
+  if (pos > 0)
+    putchar('\n');
+}
+
+/* Expand tab into blanks */
+int exptab(int pos) {
+  line[pos++] = ' '; /* tab is at least one blank */
+
+  for (; pos < MAXCOL && pos % TABINC != 0; ++pos)
+    line[pos] = ' ';
+
+  if (pos < MAXCOL) /* room left in current line */
+    return pos;
+  else {
+    /* current line is full */
+    printl(pos);
+    return 0; /* reset current position */
+  }
+}
+
+int findblnk(int pos) {
+  while (pos > 0 && line[pos] != ' ')
+    --pos;
+
+  if (pos == 0) /* no blanks in the line? */
+    return MAXCOL;
+  else              /* at least one blank */
+    return pos + 1; /* position after the blank */
+}
+
+/* Rearrange line with new position */
+int newpos(int pos) {
+  int i, j;
+
+  if (pos <= 0 || pos >= MAXCOL)
+    return 0; /* nothing to rearrange */
+  else {
+    i = 0;
+    for (j = pos; j < MAXCOL; ++j) {
+      line[i] = line[j];
+      ++i;
+    }
+    return i; /* new position in line */
   }
 }
