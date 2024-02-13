c85ac3dfba60594f1191203a27e47cab1400c56b
viktor <camalouu@gmail.com>
2024-02-13 14:29:36 +0500

    1.23 and 1.24 left

---
+++ chapter_1/1.24.c
@@ -0,0 +1,10 @@
+/*
+    Write a program to check a C program for rudimentary syntax
+    errors like unbalanced parentheses, brackets and braces. Don’t forget about
+    quotes, both single and double, escape sequences, and comments. (This pro-
+    gram is hard if you do it in full generality.)
+*/
+
+#include <stdio.h>
+
+int main() { return 0; }
--- chapter_1/test.c
+++ chapter_1/test.c
@@ -3,46 +3,7 @@
   Don’t forget to handle quoted strings and character constants properly. C com-
   ments do not nest.
 */
+
 #include <stdio.h>
 
-#define IN 1
-#define OUT 0
-
-int getline_(char line[], int maxlength) {
-  int c, i;
-  for (i = 0; i < maxlength - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
-    line[i] = c;
-  }
-
-  if (c == '\n')
-    line[i++] = c;
-
-  line[i] = '\0';
-
-  return i;
-}
-
-void removeCommentInLine(char line[], int len) {
-  int state = OUT;
-  for (int i = 0; i < len - 1; i++) {
-    if (line[i] == '\'' || line[i] == '\"') {
-      state = (state == OUT) ? IN : OUT;
-    } else if (state == IN) {
-      continue;
-    } else if (line[i] == '/' && line[i + 1] == '/' && state == OUT) {
-      line[i] = '\n';
-      line[i + 1] = '\0';
-      return;
-    }
-  }
-}
-
-int main() {
-  char line[1000];
-  int len;
-  while ((len = getline_(line, 1000)) > 0) {
-    removeCommentInLine(line, len);
-    printf("%s", line);
-  }
-  return 0;
-}
+int main() { return 0; }
